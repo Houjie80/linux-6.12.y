a817eeda6d05c718de18dcd9cbc5dbec1abd54f8
Houjie80 <86527356+Houjie80@users.noreply.github.com>
2025-03-15 21:23:25 +0700

    Update pwm-meson.c

--- drivers/pwm/pwm-meson.c
+++ drivers/pwm/pwm-meson.c
@@ -37,6 +37,7 @@
 #include <linux/math64.h>
 #include <linux/module.h>
 #include <linux/of.h>
+#include <linux/of_device.h>
 #include <linux/platform_device.h>
 #include <linux/pwm.h>
 #include <linux/slab.h>
