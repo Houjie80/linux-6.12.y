0e62100a1d079cdc73d241b0313d74be5531bf41
Houjie80 <86527356+Houjie80@users.noreply.github.com>
2025-03-20 08:04:19 +0700

    Update i2c-gpio.c

--- drivers/i2c/busses/i2c-gpio.c
+++ drivers/i2c/busses/i2c-gpio.c
@@ -445,12 +445,12 @@ static int i2c_gpio_probe(struct platform_device *pdev)
 
 	adap->owner = THIS_MODULE;
 	if (np)
-		strlcpy(adap->name, dev_name(dev), sizeof(adap->name));
+		strscpy(adap->name, dev_name(dev), sizeof(adap->name));
 	else
 		snprintf(adap->name, sizeof(adap->name), "i2c-gpio%d", pdev->id);
 
 	adap->algo_data = bit_data;
-	adap->class = I2C_CLASS_HWMON | I2C_CLASS_SPD;
+	adap->class = I2C_CLASS_HWMON;
 	adap->dev.parent = dev;
 	adap->dev.of_node = np;
 
